735b97f47b35abb7fb70d1d7091ce7b0f9e11aea
Sandesh Wankhade <104431880+thepeakyblinderr@users.noreply.github.com>
2026-03-15 16:35:09 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'splitvault-v4';
-const ASSETS = ['./index.html', './manifest.json'];
+const CACHE = 'moneymate-v1';
+const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
   self.skipWaiting();
@@ -7,6 +7,7 @@ self.addEventListener('install', e => {
 });
 
 self.addEventListener('activate', e => {
+  // Delete ALL old caches (splitvault-v4 etc.)
   e.waitUntil(
     caches.keys().then(keys =>
       Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
